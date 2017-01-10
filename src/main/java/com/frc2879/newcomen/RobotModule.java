d0c93a9bd4ac5b4ee4951d75681a2d2d5b6f99b7
Paul Friederichsen <floogulinc@gmail.com>
2017-01-10 16:20:30 -0600

    Initial project setup

---
+++ src/main/java/com/frc2879/newcomen/RobotModule.java
@@ -0,0 +1,25 @@
+package com.frc2879.newcomen;
+
+import jaci.openrio.toast.lib.log.Logger;
+import jaci.openrio.toast.lib.module.IterativeModule;
+
+public class RobotModule extends IterativeModule {
+
+    public static Logger logger;
+
+    @Override
+    public String getModuleName() {
+        return "newcomen";
+    }
+
+    @Override
+    public String getModuleVersion() {
+        return "0.0.1";
+    }
+
+    @Override
+    public void robotInit() {
+        logger = new Logger("newcomen", Logger.ATTR_DEFAULT);
+        //TODO: Module Init
+    }
+}
