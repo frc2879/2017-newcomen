9732a33fd866e43f8a743235054c6ed9e5fcfc3e
rfavour <rfavour9@gmail.com>
2017-01-11 21:09:45 -0600

    Talons and simple mecanum drive
    using old xbox code for now, set temporary configs for talons in
    drivetrain subsystem

--- src/main/java/com/frc2879/newcomen/commands/DriveMecanum.java
+++ src/main/java/com/frc2879/newcomen/commands/DriveMecanum.java
@@ -1,17 +1,23 @@
 package com.frc2879.newcomen.commands;
 
 import com.frc2879.newcomen.RobotModule;
+import com.frc2879.xboxcontroller.XboxController;
 
 import edu.wpi.first.wpilibj.command.Command;
+
 
 /**
  */
 public class DriveMecanum extends Command{
-    public DriveMecanum() {
+	
+	public final XboxController driveJoystick = new XboxController(0); // move to oi later
+    
+	public DriveMecanum(){
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
         super("DriveMecanum");
         requires(RobotModule.drivetrain);
+       
     }
 
     // Called just before this Command runs the first time
@@ -20,6 +26,7 @@ public class DriveMecanum extends Command{
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	RobotModule.drivetrain.getRobotDrive().mecanumDrive_Cartesian(driveJoystick.leftStick.getX(), driveJoystick.leftStick.getY(), driveJoystick.rightStick.getX(),0);
     }
 
     // Make this return true when this Command no longer needs to run execute()
