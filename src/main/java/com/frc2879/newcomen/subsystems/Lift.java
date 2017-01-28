27685ebcedad17ee8d4e4ff095c9edc56bfc3697
Paul Friederichsen <floogulinc@gmail.com>
2017-01-28 14:59:37 -0600

    Begin to add lift

--- src/main/java/com/frc2879/newcomen/subsystems/Lift.java
+++ src/main/java/com/frc2879/newcomen/subsystems/Lift.java
@@ -1,5 +1,7 @@
 package com.frc2879.newcomen.subsystems;
 
+import com.ctre.CANTalon;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -11,20 +13,30 @@ public class Lift extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
+	public static int TALONS_LIFT = 4;
+	
+	CANTalon liftTalon;
 
     public Lift() {
-        super("Drivetrain");
+        super("Lift");
         SmartDashboard.putData(this);
-
-
+        
+        liftTalon = new CANTalon(TALONS_LIFT);
     }
-
-
 
     public void initDefaultCommand() {
 
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
+    
+    public void set(double outputValue) {
+        liftTalon.set(outputValue);
+    }
+    
+    public void stop() {
+    	liftTalon.disableControl();
+    }
+    
 
 }
